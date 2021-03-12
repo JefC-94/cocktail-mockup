fbfde4bdb8820303a36e469adb2994076afbcd08
Jef <studioz@MacBook-Pro-van-Jef-2.local>
2021-03-12 16:18:27 +0100

    fix grid + random cocktail base

--- src/App.js
+++ src/App.js
@@ -30,7 +30,7 @@ function App() {
         <Container>
           <Heading as="h1" size="xl" m='5' align="center">Cocktails 4 Life</Heading>
         </Container>
-        <Container flex="1" p="0" border="0" borderColor="white" w="95%" maxW="1000px" minH='600px'>
+        <Container flex="1" p="0" border="0" borderColor="white" w="95%" maxW="1200px" minH='600px'>
           <Flex border="0" borderColor="white">
           <Box w="75%">
             <Container border="2px" borderColor="gray.300" borderRadius="5px" maxW="" w="100%" p="1em">
@@ -56,44 +56,49 @@ function App() {
               </form>
             
             </Container>
-            <Grid mt="1em" templateColumns="repeat(4,1fr)" gap={6}>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+            <Grid mt="1em" templateColumns="repeat(4,1fr)" templateRows="repeat(2, auto)" gap='8'>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
-              <Box h="200px">
-                <Image boxSize="160px" objectFit='cover' src={cocktailjpg} />
-                <Center mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Center>
+              <Box>
+                <Image w="100%" objectFit='cover' src={cocktailjpg} />
+                <Text align="center" mt='1' fontWeight='600' textTransform="uppercase" >Cocktail Name</Text>
               </Box>
             </Grid> 
           </Box>
         <Divider w="2em" />
-          <Box w="25%" bg="green">
-            <Text >Joe</Text>
+          <Box w="25%">
+            <Text mb="2">Random cocktail</Text>
+            <Image objectFit="cover" src={cocktailjpg} />
+            <Text fontSize="md" mt="2" mb="4" >Margarita on the rocks</Text>
+            <Text fontSize="sm" >Some text</Text>
+            <Text fontSize="sm" mt="2" mb="2">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Etiam malesuada, urna nec congue gravida, leo ante mollis erat, at mattis dui nibh vitae ex. Aliquam maximus lorem tristique nisi luctus volutpat. Interdum et malesuada fames ac ante ipsum primis in faucibus.</Text>
+            <Text fontSize="sm" >Some text</Text>
           </Box>
         </Flex> 
         </Container>
